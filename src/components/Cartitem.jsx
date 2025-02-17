652a095d0d7c9b09d3adf56797508a69c273d23f
chaudhary shoeb <58807859+shoeb145@users.noreply.github.com>
2025-02-17 23:57:12 +0530

    setup

---
+++ src/components/Cartitem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Cartitem(props) {
+  return <div></div>;
+}
+
+export default Cartitem;
---
+++ src/components/Productcard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Productcard(props) {
+  return <div></div>;
+}
+
+export default Productcard;
